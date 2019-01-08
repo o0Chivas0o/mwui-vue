679ea290d8b80069432c9c4ff09fdac22840e080
Lee <326656669@qq.com>
2019-01-08 18:02:06 +0800

    feat : 更新node_modules
    - 修改.gitignore
    - 修改index.html

---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="w-button">按钮</button>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss">
+    .w-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
