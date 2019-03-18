004522b24a9a75f5cea368f71aaea6ab781915d1
Lee <326656669@qq.com>
2019-03-18 11:06:04 +0800

    feat : add puppeteer

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function ( config ) {
     reporters:['progress'],
     
     // web server port
-    port:9876,
+    port:8888,
     
     // enable / disable colors in the output (reporters and logs)
     colors:true,
