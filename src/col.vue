1b7a9b82bffbd17202cb6b3a3434cf4af024058a
Lee <326656669@qq.com>
2019-04-09 17:55:16 +0800

    refactor : To simplify the code
    - row.vue
    - col.vue

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="col" :class="[span && `col-${span}` , offset && `offset-${offset}`]"
-       :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+  <div class="col" :class="colClass" :style="colStyle">
     <slot></slot>
   </div>
 </template>
@@ -14,7 +13,23 @@
     },
     data () {
       return {
-        gutter: 0
+        gutter: 0,
+      }
+    },
+    computed: {
+      colClass () {
+        let {span, offset} = this
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`
+        ]
+      },
+      colStyle () {
+        let {gutter} = this
+        return {
+          paddingLeft: gutter / 2 + 'px',
+          paddingRight: gutter / 2 + 'px'
+        }
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="{paddingLeft:-gutter/2+'px',paddingRight:-gutter/2+'px'}">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
@@ -10,8 +10,17 @@
     props: {
       gutter: {type: [Number, String]}
     },
+    computed: {
+      rowStyle () {
+        let {gutter} = this
+        return {
+          paddingLeft: -gutter / 2 + 'px',
+          paddingRight: -gutter / 2 + 'px'
+        }
+      }
+    },
     mounted () {
-      this.$children.forEach((vm)=>{
+      this.$children.forEach((vm) => {
         vm.gutter = this.gutter
       })
     }
