665cf1ba4de2d0dc3e136710407e18bc17d94ade
Lee <326656669@qq.com>
2019-06-18 16:01:05 +0800

    feat : add adnimation & fixed some time will close toast twice

--- src/plugin.js
+++ src/plugin.js
@@ -6,16 +6,17 @@ export default {
   install (Vue, options) {
     Vue.prototype.$toast = function (message, toastOptions) {
       if (currentToast) {currentToast.close()}
-      currentToast = createToast({Vue, message, propsData: toastOptions})
+      currentToast = createToast({Vue, message, propsData: toastOptions, onClose: () => {currentToast = null}})
     }
   }
 }
 
-function createToast ({Vue, message, propsData}) {
+function createToast ({Vue, message, propsData, onClose}) {
   let Constructor = Vue.extend(Toast)
   let toast = new Constructor({propsData})
   toast.$slots.default = [message]
   toast.$mount() // 必须mount 否则生命周期都不会实现
+  this.$on('close', onClose)
   document.body.appendChild(toast.$el)
   return toast
 }
--- src/toast.vue
+++ src/toast.vue
@@ -51,6 +51,7 @@
       },
       close () {
         this.$el.remove()
+        this.$emit('close')
         this.$destroy()
       },
       onClickClose () {
@@ -68,8 +69,8 @@
   $toast-min-height: 40px;
   $toast-bg: rgba(0, 0, 0, .75);
   @keyframes fade-in {
-    0% {opacity: 0;}
-    100% {opacity: 100%;}
+    0% {opacity: 0;transform: translateY(100%)}
+    100% {opacity: 100%;transform: translateY(0%)}
   }
   .toast {
     animation: fade-in 1s;
