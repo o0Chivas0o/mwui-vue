0edd3b3f0dabb3774e4d84bf4e02b4149c55b863
Lee <326656669@qq.com>
2019-07-01 11:00:06 +0800

    fixed : style of undefined in No.3 test file

--- test/toast.test.js
+++ test/toast.test.js
@@ -25,14 +25,17 @@ describe('Toast', () => {
       })
     })
     
-    it('接受 closeButton', () => {
+    it('接受 closeButton', (done) => {
       const callback = sinon.fake()
       const Constructor = Vue.extend(Toast)
       const vm = new Constructor({propsData: {closeButton: {text: '关闭吧', callback}}}).$mount()
       let closeButton = vm.$el.querySelector('.close')
       expect(closeButton.textContent.trim()).to.eq('关闭吧')
-      closeButton.click()
-      expect(callback).to.have.been.called
+      setTimeout(() => {
+        closeButton.click()
+        expect(callback).to.have.been.called
+        done()
+      })
     })
     
     it('接受 enableHtml', () => {
