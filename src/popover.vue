44bb5cd13ffa7745ea2d4369c13ae7f972fdb125
Lee <326656669@qq.com>
2019-07-04 18:25:32 +0800

    feat : 修复 popover 位置 并将 contentWrapper 移到body最后一个元素

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="popover" @click="xxx">
-    <div class="content-wrapper" v-if="visible">
+  <div class="popover" @click.stop="xxx">
+    <div class="content-wrapper" v-if="visible" ref="contentWrapper" @click.stop>
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -18,6 +20,19 @@
     methods: {
       xxx () {
         this.visible = !this.visible
+        if (this.visible) {
+          let eventHandler = () => {
+            this.visible = false
+            document.removeEventListener('click', eventHandler)
+          }
+          this.$nextTick(() => {
+            let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left + 'px'
+            this.$refs.contentWrapper.style.top = top + 'px'
+            document.body.appendChild(this.$refs.contentWrapper)
+            document.addEventListener('click', eventHandler)
+          })
+        }
       }
     }
   }
@@ -28,11 +43,10 @@
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper {
-      position: absolute;
-      bottom: 100%;
-      left: 0;
-      box-shadow: 0 0 3px rgba(0, 0, 0, .5)
-    }
+  }
+  .content-wrapper {
+    position: absolute;
+    box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+    transform: translateY(-150%);
   }
 </style>
