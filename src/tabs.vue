7432928b3e6dcd420161c42826e11437c51a2b72
Lee <326656669@qq.com>
2019-07-08 10:50:26 +0800

    feat : Optimize code logic

--- src/tabs.vue
+++ src/tabs.vue
@@ -29,20 +29,28 @@
         eventBus: this.eventBus
       }
     },
-    mounted () {
-      if (this.$children.length === 0) {
-        console && console.warn &&
-        console.warn('tabs的子组件应该是tabs-nav和tabs-content,但是没有子组件 ')
-      }
-      this.$children.forEach((vm) => {
-        if (vm.$options.name === 'WTabsNav') {
-          vm.$children.forEach((item) => {
-            if (item.$options.name === 'WTabsItem' && item.name === this.selected) {
-              this.eventBus.$emit('update:selected', this.selected, item)
-            }
-          })
+    methods: {
+      checkChildren () {
+        if (this.$children.length === 0) {
+          console && console.warn &&
+          console.warn('tabs的子组件应该是tabs-nav和tabs-content,但是没有子组件 ')
         }
-      })
+      },
+      selectTab () {
+        this.$children.forEach((vm) => {
+          if (vm.$options.name === 'WTabsNav') {
+            vm.$children.forEach((item) => {
+              if (item.$options.name === 'WTabsItem' && item.name === this.selected) {
+                this.eventBus.$emit('update:selected', this.selected, item)
+              }
+            })
+          }
+        })
+      }
+    },
+    mounted () {
+      this.checkChildren()
+      this.selectTab()
     }
   }
 </script>
