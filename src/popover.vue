7e87c299e2812468a97690c2cd5f0c717f33c23f
Lee <326656669@qq.com>
2019-07-08 17:56:57 +0800

    feat : 添加插槽的 close api

--- src/popover.vue
+++ src/popover.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="popover" ref="popover">
     <div class="content-wrapper" v-if="visible" ref="contentWrapper" :class="{[`position-${position}`]:true}">
-      <slot name="content"></slot>
+      <slot name="content" :close="close"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block;">
       <slot></slot>
@@ -12,7 +12,7 @@
 <script>
   export default {
     name: 'WPopover',
-    data () {return {visible: false,}},
+    data () {return {visible: false, timer: null}},
     props: {
       position: {
         type: String,
@@ -41,19 +41,28 @@
       if (this.trigger === 'click') {
         this.$refs.popover.addEventListener('click', this.onClick)
       } else {
-        this.$refs.popover.addEventListener('mouseenter', this.open)
-        this.$refs.popover.addEventListener('mouseleave', this.close)
+        this.$refs.popover.addEventListener('mouseenter', this.hoverOpen)
+        this.$refs.popover.addEventListener('mouseleave', this.hoverClose)
       }
     },
     destroyed () {
       if (this.trigger === 'click') {
         this.$refs.popover.removeEventListener('click', this.onClick)
       } else {
-        this.$refs.popover.removeEventListener('mouseenter', this.open)
-        this.$refs.popover.removeEventListener('mouseleave', this.close)
+        this.$refs.popover.removeEventListener('mouseenter', this.hoverOpen)
+        this.$refs.popover.removeEventListener('mouseleave', this.hoverClose)
       }
     },
     methods: {
+      onClick (event) {
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          if (!this.visible) {
+            this.open()
+          } else {
+            this.close()
+          }
+        }
+      },
       positionContent () {
         const {contentWrapper, triggerWrapper} = this.$refs
         document.body.appendChild(contentWrapper)
@@ -73,6 +82,25 @@
         if (this.$refs.contentWrapper && (this.$refs.contentWrapper.contains(e.target) || this.$refs.contentWrapper === e.target)) {return}
         this.close()
       },
+      hoverOpen () {
+        this.clearTimer()
+        this.visible = true
+        this.$nextTick(() => {
+          const {contentWrapper} = this.$refs
+          this.positionContent()
+          contentWrapper.addEventListener('mouseenter', this.hoverOpen)
+          contentWrapper.addEventListener('mouseleave', this.hoverClose)
+        })
+      },
+      hoverClose () {
+        const {contentWrapper} = this.$refs
+        this.timer = setTimeout(() => {
+          this.visible = false
+          this.timer = null
+          contentWrapper.removeEventListener('mouseenter', this.open)
+          contentWrapper.removeEventListener('mouseleave', this.close)
+        }, 200)
+      },
       open () {
         this.visible = !this.visible
         this.$nextTick(() => {
@@ -84,13 +112,10 @@
         this.visible = false
         document.removeEventListener('click', this.onClickDocument)
       },
-      onClick (event) {
-        if (this.$refs.triggerWrapper.contains(event.target)) {
-          if (!this.visible) {
-            this.open()
-          } else {
-            this.close()
-          }
+      clearTimer () {
+        if (this.timer) {
+          clearTimeout(this.timer)
+          this.timer = null
         }
       }
     }
@@ -104,31 +129,31 @@
   .content-wrapper {
     position: absolute;background: white; /*box-shadow: 0 0 3px rgba(0, 0, 0, .5);*/
     filter: drop-shadow(0 1px 1px rgba(0, 0, 0, .5));
-    border: 1px solid $border-color;border-radius: $border-radius;padding: .5em 1em;max-width: 20em;word-break: break-all;
+    border: 1px solid $border-color;border-radius: $border-radius;padding: .5em 1em;max-width: 24em;word-break: break-all;
     &::before, &::after {content: '';display: block;width: 0;height: 0;border: 10px solid transparent;position: absolute;}
     &.position-top {
       margin-top: -10px;
       transform: translateY(-100%);
-      &::before, &::after {left: 10px;border-bottom:none;}
+      &::before, &::after {left: 10px;border-bottom: none;}
       &::before {top: 100%;border-top-color: black;}
       &::after {top: calc(100% - 1px);border-top-color: white;}
     }
     &.position-bottom {
       margin-top: 10px;
-      &::before, &::after {left: 10px;border-top:none;}
+      &::before, &::after {left: 10px;border-top: none;}
       &::before {bottom: 100%;border-bottom-color: black;}
       &::after {bottom: calc(100% - 1px);border-bottom-color: white;}
     }
     &.position-left {
       margin-left: -10px;
       transform: translateX(-100%);
-      &::before, &::after {left: 100%;top: 50%;transform: translateY(-50%);border-right:none;}
+      &::before, &::after {left: 100%;top: 50%;transform: translateY(-50%);border-right: none;}
       &::before {border-left-color: black;}
       &::after {border-left-color: white;left: calc(100% - 1px);}
     }
     &.position-right {
       margin-left: 10px;
-      &::before, &::after {right: 100%;top: 50%;transform: translateY(-50%);border-left:none;}
+      &::before, &::after {right: 100%;top: 50%;transform: translateY(-50%);border-left: none;}
       &::before {border-right-color: black;}
       &::after {border-right-color: white;right: calc(100% - 1px);}
     }
