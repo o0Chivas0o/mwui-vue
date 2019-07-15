7fcb5a6c85610edb57fc3d67caa32c0be5771659
Lee <326656669@qq.com>
2019-07-15 15:09:17 +0800

    feat : 确定如何展示内容

---
+++ src/cascader-items.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="cascaderItems">
+    <div class="left">
+      <div class="label" v-for="item in items" @click="leftSelected = item">
+        {{item.name}}
+      </div>
+    </div>
+    <div class="right" v-if="rightItems">
+      <w-cascader-item :items="rightItems"></w-cascader-item>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'WCascaderItem',
+    props: {
+      items: {type: Array}
+    },
+    data () {
+      return {
+        leftSelected: null
+      }
+    },
+    computed: {
+      rightItems () {
+        return this.leftSelected && this.leftSelected.children ? this.leftSelected.children : null
+      }
+    }
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  .cascaderItems {
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    .left {
+      border: 1px solid red;
+    }
+    .right {
+      margin-top: -1px;
+    }
+  }
+</style>
--- src/cascader.vue
+++ src/cascader.vue
@@ -1,30 +1,54 @@
 <template>
   <div class="cascader">
-    <div class="trigger">
+    <div class="trigger" @click="popoverVisible = !popoverVisible">
       <slot></slot>
     </div>
-    <div class="popover">
-      <div v-for="item in source">
-        <w-cascader-item :sourceItem="item"></w-cascader-item>
-      </div>
+    <div class="popover" v-if="popoverVisible">
+      <cascader-items :items="source"></cascader-items>
     </div>
   </div>
 </template>
 
 <script>
-  import WCascaderItem from './cascader-item'
+  import CascaderItems from './cascader-items'
+  
   export default {
     name: 'WCascader',
-    components: {WCascaderItem},
-    props: {
-      source: {
-        type: Array
+    components: {CascaderItems},
+    props: {source: {type: Array}},
+    data () {
+      return {
+        popoverVisible: false,
+        level1Selected: null,
+        level2Selected: null
       }
     },
+    computed: {
+      level1Items () {
+        return this.level1Selected ? this.level1Selected.children : []
+      },
+      level2Items () {
+        return this.level2Selected ? this.level2Selected.children : []
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
   @import 'var';
-
+  .cascader {
+    .trigger {
+      border: 1px solid red;
+      width: 100px;
+      height: 30px;
+    }
+    .popover {
+      height: 300px;
+      display: flex;
+      border: 1px solid green;
+      .label {
+        border: 1px solid black;
+      }
+    }
+  }
 </style>
