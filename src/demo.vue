e0cb559808088ea952bc56f87f9a70727294e8e6
Lee <326656669@qq.com>
2019-07-16 17:06:59 +0800

    feat : 使用vue-cli 构建项目

---
+++ src/demo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <cascader :source="source" popoverHeight="200px"></cascader>
+  </div>
+</template>
+
+<script>
+  import Cascader from './cascader'
+  
+  export default {
+    name: 'demo',
+    components: {Cascader},
+    data () {
+      return {
+        source: [
+          {
+            name: '湖北',
+            children: [
+              {
+                name: '武汉',
+                children: [{name: '武昌'}, {name: '汉阳'}, {name: '汉口'}]
+              }
+            ]
+          },
+          {
+            name: '湖南',
+            children: [
+              {
+                name: '长沙',
+                children: [{name: '1区'}, {name: '2区'}, {name: '3区'}]
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
