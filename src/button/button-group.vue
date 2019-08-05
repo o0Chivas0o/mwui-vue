f797f78028b4dda7fe559c7c39561fa74d611faa
Lee <326656669@qq.com>
2019-08-05 10:38:14 +0800

    feat : change var(--border-radius) to scss

--- src/button/button-group.vue
+++ src/button/button-group.vue
@@ -17,13 +17,15 @@
   }
 </script>
 <style lang="scss" scoped>
+  @import '@/_var.scss';
+  
   .w-button-group {
     display: inline-flex;vertical-align: middle;
     > .w-button {
       border-radius: 0;
       &:not(:first-child) { margin-left: -1px;}
-      &:first-child {border-top-left-radius: var(--border-radius);border-bottom-left-radius: var(--border-radius);}
-      &:last-child {border-top-right-radius: var(--border-radius);border-bottom-right-radius: var(--border-radius);}
+      &:first-child {border-top-left-radius: $border-radius;border-bottom-left-radius: $border-radius;}
+      &:last-child {border-top-right-radius: $border-radius;border-bottom-right-radius: $border-radius;}
       &:hover {
         position: relative;
         z-index: 1;
