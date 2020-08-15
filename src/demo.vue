ac0fbb9c5bfd7ee3292f1181e5316c6f590d1cca
Lee <326656669@qq.com>
2020-08-16 01:28:18 +0800

    feat : update

--- src/demo.vue
+++ src/demo.vue
@@ -5,7 +5,7 @@
                expend-field="description" :height="200" :selected-items.sync="selected" :loading="loading">
         <w-table-column text="姓名" field="name" :width="100">
           <template slot-scope="props">
-            <a href="#">{{props.value}}</a>
+            <a href="#">{{ props.value }}</a>
           </template>
         </w-table-column>
         <w-table-column text="分数" field="score" :width="100"></w-table-column>
@@ -18,64 +18,64 @@
 </template>
 
 <script>
-  import WPagination from '@/pagination/pagination'
-  import WTableColumn from '@/table/table-column'
-  import WTable from '@/table/table'
-  import WButton from '@/button/button'
-  
-  export default {
-    name: 'demo',
-    components: {WPagination, WTable, WButton, WTableColumn},
-    data () {
-      return {
-        currentPage: 1,
-        selected: [],
-        loading: false,
-        orderBy: {
-          name: true,
-          score: 'desc'
-        },
-        dataSource: [
-          {id: 11, name: 'zjl', score: 100, description: 'xxx'},
-          {id: 22, name: 'lzy', score: 99, description: 'yyy'},
-          {id: 33, name: 'zjl', score: 98},
-          {id: 44, name: 'lzy', score: 97},
-          {id: 55, name: 'zjl', score: 96},
-          {id: 66, name: 'lzy', score: 95},
-          {id: 77, name: 'zjl', score: 100},
-          {id: 88, name: 'lzy', score: 99},
-          {id: 99, name: 'zjl', score: 98},
-          {id: 12, name: 'lzy', score: 97},
-          {id: 13, name: 'zjl', score: 96},
-          {id: 14, name: 'lzy', score: 95},
-          {id: 15, name: 'zjl', score: 100},
-          {id: 16, name: 'lzy', score: 99},
-          {id: 17, name: 'zjl', score: 98},
-          {id: 18, name: 'lzy', score: 97},
-          {id: 19, name: 'zjl', score: 96},
-          {id: 21, name: 'lzy', score: 95},
-        ]
-      }
+import WPagination from '@/pagination/pagination'
+import WTableColumn from '@/table/table-column'
+import WTable from '@/table/table'
+import WButton from '@/button/button'
+
+export default {
+  name: 'demo',
+  components: { WPagination, WTable, WButton, WTableColumn },
+  data() {
+    return {
+      currentPage: 1,
+      selected: [],
+      loading: false,
+      orderBy: {
+        name: true,
+        score: 'desc'
+      },
+      dataSource: [
+        { id: 11, name: 'zjl', score: 100, description: 'xxx' },
+        { id: 22, name: 'lzy', score: 99, description: 'yyy' },
+        { id: 33, name: 'zjl', score: 98 },
+        { id: 44, name: 'lzy', score: 97 },
+        { id: 55, name: 'zjl', score: 96 },
+        { id: 66, name: 'lzy', score: 95 },
+        { id: 77, name: 'zjl', score: 100 },
+        { id: 88, name: 'lzy', score: 99 },
+        { id: 99, name: 'zjl', score: 98 },
+        { id: 12, name: 'lzy', score: 97 },
+        { id: 13, name: 'zjl', score: 96 },
+        { id: 14, name: 'lzy', score: 95 },
+        { id: 15, name: 'zjl', score: 100 },
+        { id: 16, name: 'lzy', score: 99 },
+        { id: 17, name: 'zjl', score: 98 },
+        { id: 18, name: 'lzy', score: 97 },
+        { id: 19, name: 'zjl', score: 96 },
+        { id: 21, name: 'lzy', score: 95 },
+      ]
+    }
+  },
+  created() {},
+  methods: {
+    order() {
+      this.loading = true
+      setTimeout(() => {
+        this.dataSource.sort((a, b) => {return a.score - b.score})
+        this.loading = false
+      }, 1000)
     },
-    created () {},
-    methods: {
-      order () {
-        this.loading = true
-        setTimeout(() => {
-          this.dataSource.sort((a, b) => {return a.score - b.score})
-          this.loading = false
-        }, 1000)
-      },
-      edit (item) {
-        alert(`${JSON.stringify(item)},编辑`)
-      },
-      view (item) {
-        alert(`${JSON.stringify(item)},查看 `)
-      }
+    edit(item) {
+      alert(`${JSON.stringify(item)},编辑`)
+    },
+    view(item) {
+      alert(`${JSON.stringify(item)},查看 `)
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  * {margin: 0;padding: 0;box-sizing: border-box}
+* {margin: 0;padding: 0;box-sizing: border-box}
 </style>
